5325e04a057840b7cd64456a321ab7801c6b3e13
AmirOdisee <programirbe@gmail.com>
2024-11-09 18:39:21 +0100

    work on Validator in DTO

---
+++ ASP.NET Web Core API/IbadahLover.Application/DTOs/UserActivity/Validators/CreateUserActivityDtoValidator.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IbadahLover.Application.DTOs.UserActivity.Validators
+{
+    public class CreateUserActivityDtoValidator : AbstractValidator<CreateUserActivityDto>
+    {
+        public CreateUserActivityDtoValidator()
+        {
+            RuleFor(p => p.UserAccountId)
+                .NotEmpty().WithMessage("{PropertyName} is required.")
+                .NotNull();
+
+            RuleFor(p => p.DhikrTypeId)
+                .NotEmpty().WithMessage("{PropertyName} is required.")
+                .NotNull();
+
+            RuleFor(p => p.TotalDhikrPerformed)
+                .NotNull()
+                .GreaterThan(0);
+
+            //RuleFor(p => p)
+            //.Custom((dto, context) =>
+            //{
+            //    var existingActivity = _activityRepository.GetActivityForToday(dto.UserAccountId, dto.DhikrTypeId);
+            //    if (existingActivity != null)
+            //    {
+            //        context.AddFailure("A record for today already exists.");
+            //    }
+            //});
+        }
+    }
+}
---
+++ ASP.NET Web Core API/IbadahLover.Application/Features/UserActivities/Handlers/Commands/CreateUserActivityCommandHandler.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using IbadahLover.Application.DTOs.UserAccount.Validators;
+using IbadahLover.Application.Features.UserAccounts.Requests.Commands;
+using IbadahLover.Application.Features.UserActivities.Requests.Commands;
+using IbadahLover.Application.Persistence.Contracts;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IbadahLover.Application.Features.UserActivities.Handlers.Commands
+{
+    public class CreateUserActivityCommandHandler : IRequestHandler<CreateUserActivityCommand, int>
+    {
+        private readonly IUserActivityRepository _userActivityRepository;
+        private readonly IMapper _mapper;
+
+        public CreateUserActivityCommandHandler(IUserActivityRepository userActivityRepository, IMapper mapper)
+        {
+            _userActivityRepository = userActivityRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<int> Handle(CreateUserActivityCommand request, CancellationToken cancellationToken)
+        {
+            var validator = new CreateUserActivityDtoValidator();
+            var validationResult = await validator.ValidateAsync(request.UserActivityDto);
+
+            //var validationResult = await validator.ValidateAsync(new CreateUserAccountDto
+            //{
+            //    // Copy properties from UserAccountDto to CreateUserAccountDto
+            //    Property1 = request.UserAccountDto.Property1,
+            //    Property2 = request.UserAccountDto.Property2,
+            //    // Add more properties as needed
+            //});
+
+            if (!validationResult.IsValid)
+            {
+                throw new Exception();
+            }
+
+            var userActivity = _mapper.Map<UserActivity>(request.UserActivityDto);
+            userActivity = await _userActivityRepository.Create(userActivity);
+
+            return userActivity.Id;
+        }
+    }
+}
