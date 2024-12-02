5d0c2a5af2be463c050934fbb54b60e77e4a9206
AmirOdisee <programirbe@gmail.com>
2024-12-02 10:46:53 +0100

    sql scribt db create klaar v1.0

--- Rest/Database/IbadahLoverDBSQLScript.sql
+++ Rest/Database/IbadahLoverDBSQLScript.sql
@@ -8,7 +8,7 @@ CREATE TABLE User_Account (
     id BIGINT PRIMARY KEY IDENTITY(1,1), -- Auto-incrementing primary key
     full_name NVARCHAR(25) NOT NULL,
     email NVARCHAR(50) NOT NULL UNIQUE, -- Ensuring email is unique
-	profile_picture IMAGE NULL, -- Base64 encryption for Project!
+	Profile_Picture_Type_id BIGINT DEFAULT 1 NOT NULL, -- Base64 encryption for Project!
     password_hash NVARCHAR(255) NULL,
 	oauth_provider NVARCHAR(20) NULL,
 	oauth_id NVARCHAR(255) NULL,
@@ -17,11 +17,9 @@ CREATE TABLE User_Account (
 	total_warnings INT DEFAULT 0 NOT NULL,
 	created_on DATE DEFAULT GETDATE() NOT NULL, -- Automatically sets to the current date
 	last_modified_on DATE DEFAULT GETDATE() NOT NULL,
-	--CHECK HERE UNDER!!!!!
-	last_modified_by BIGINT NOT NULL, --HOW TO LET CREATED ID IN HERE??? TRIGGER?????
+	last_modified_by BIGINT NULL, --do i add TRIGGER?????
 
-	--CHeck foreign key contstraiint!!!!!
-	CONSTRAINT FK_User_Account_last_modified_by FOREIGN KEY (last_modified_by) REFERENCES User_Account(id), -- SAAME HERE!!! check:!!!
+	CONSTRAINT FK_User_Account_last_modified_by FOREIGN KEY (last_modified_by) REFERENCES User_Account(id),
 	CONSTRAINT UQ_User_Account_email UNIQUE (email),
 	CONSTRAINT UQ_User_Account_email_password_hash UNIQUE (email, password_hash),
 	CONSTRAINT UQ_User_Account_email_oauth_id UNIQUE (email, oauth_id),
@@ -118,7 +116,7 @@ CREATE TABLE Salah_Type (
 GO
 CREATE TABLE Profile_Picture_Type (
     id BIGINT PRIMARY KEY IDENTITY(1,1), -- Auto-incrementing primary key
-    base64_code NVARCHAR(255) NOT NULL, -- fajr, sobh, dohr, maghreb, isha, witr etc...
+    base64_code IMAGE NOT NULL, -- it takes to much space but i allow this instead of just link to image storing because restricted to limited amount of image to not deal with censoring because it is a religious app it must be heavily heavily heavily censorized (leaderboard system!) and i can't use gravatar service for exemple because they and all other services that i know don't censor the way it islamicly must!
 	created_on DATE DEFAULT GETDATE() NOT NULL, -- Automatically sets to the current date
 	created_by BIGINT NOT NULL, -- So user can create his own personal dhirk types just for him
 	last_modified_on DATE DEFAULT GETDATE() NOT NULL,
@@ -126,7 +124,7 @@ CREATE TABLE Profile_Picture_Type (
 
 	CONSTRAINT FK_Profile_Picture_Type_created_by FOREIGN KEY (created_by) REFERENCES User_Account(id),
 	CONSTRAINT FK_Profile_Picture_Type_last_modified_by FOREIGN KEY (last_modified_by) REFERENCES User_Account(id),
-	CONSTRAINT UQ_Profile_Picture_Type_base64_code UNIQUE (base64_code) -- Ensures a unique record per Salah type.
+	-- CONSTRAINT UQ_Profile_Picture_Type_base64_code UNIQUE (base64_code) -- Ensures a unique record per Salah type. Couldn't do it cause cannot index on image type to long characters, chatgpt propose me to hash the base64_code and that I could put unique constraint on hash but I will just skip unique constraint!
 );
 GO
 CREATE TABLE User_Dhikr_Activity (
@@ -135,7 +133,7 @@ CREATE TABLE User_Dhikr_Activity (
     Dhikr_Type_id BIGINT NOT NULL, -- Foreign key to Dhikr table
     performed_on DATE DEFAULT CONVERT(VARCHAR(10), GETDATE(), 120) NOT NULL, -- The date in YYYY-MM-DD format in which the activity occurred
 	last_performed_at DATETIME DEFAULT GETDATE() NOT NULL,
-    total_performed BIGINT DEFAULT 0 NOT NULL, -- Default count to 0 for new records
+    total_performed BIGINT DEFAULT 1 NOT NULL, -- Default count to 0 for new records
     
     CONSTRAINT FK_User_Dhikr_Activity_User_Account_id FOREIGN KEY (User_Account_id) REFERENCES User_Account(id) ON DELETE CASCADE,
     CONSTRAINT FK_User_Dhikr_Activity_Dhikr_Type_id FOREIGN KEY (Dhikr_Type_id) REFERENCES Dhikr_Type(id) ON DELETE CASCADE,
@@ -153,13 +151,14 @@ CREATE TABLE User_Salah_Activity (
     CONSTRAINT FK_User_Salah_Activity_Salah_Type_id FOREIGN KEY (Salah_Type_id) REFERENCES Salah_Type(id) ON DELETE CASCADE,
     CONSTRAINT UQ_User_Salah_Activity_User_Account_id_performed_on UNIQUE (User_Account_id, performed_on) -- Ensures a unique record per user per day
 );
-l
+
 GO
 CREATE TABLE User_Salah_Overview (
     id BIGINT PRIMARY KEY IDENTITY(1,1), -- Auto-incrementing primary key
     User_Account_id BIGINT NOT NULL, -- Foreign key to Users table
     --average_punctuality_percentage DECIMAL(5,2) DEFAULT 0 NOT NULL, -- Average punctuality percentage
-	total_tracked INT DEFAULT 1 NOT NULL, -- Total of salah records taken into account for the average punctuality percentage
+	total_tracked INT DEFAULT 0 NOT NULL, -- Total of salah records taken into account for the average punctuality percentage
+	last_tracked_at DATETIME DEFAULT GETDATE() NOT NULL,
     
     CONSTRAINT FK_User_Salah_Overview_User_Account_id FOREIGN KEY (User_Account_id) REFERENCES User_Account(id) ON DELETE CASCADE
 );
@@ -218,32 +217,131 @@ CREATE TABLE User_Dhikr_Overview (
 --);
 
 --Chatgpt4o mini
+--GO
+--CREATE TRIGGER Trigger_Insert_Dhikr_Overview on User_Dhikr_Activity
+--	AFTER UPDATE
+--AS
+--BEGIN
+--	SET NOCOUNT ON;
+--	DECLARE @User_Account_id BIGINT
+--	DECLARE @total_performed BIGINT
+--    SELECT @User_Account_id = INSERTED.User_Account_id
+--	FROM INERTED
+
+--	IF UPDATE(total_performed)
+--		SET total_performed += 1
+    
+--	WHERE User_Account_id = NEW.User_Account_id;
+--END;
+--GO
+
+--CREATE TRIGGER Trigger_Update_Dhikr_Overview on User_Dhikr_Activity
+--AFTER UPDATE
+--FOR EACH ROW
+--BEGIN
+--    UPDATE User_Dhikr_Overview
+--    SET total_performed += 1
+--    WHERE User_Account_id = NEW.User_Account_id;
+--END;
+
+
+--TRIGGER FOR TOTAL TRACKED IN USER SALAH OVERVIEW BY CHATGPT
 GO
-CREATE TRIGGER Trigger_Insert_Dhikr_Overview on User_Dhikr_Activity
-	AFTER UPDATE
+
+CREATE TRIGGER Trigger_Update_User_Salah_Overview_total_tracked
+ON User_Salah_Activity
+AFTER INSERT
 AS
 BEGIN
-	SET NOCOUNT ON;
-	DECLARE @User_Account_id BIGINT
-	DECLARE @total_performed BIGINT
-    SELECT @User_Account_id = INSERTED.User_Account_id
-	FROM INERTED
-
-	IF UPDATE(total_performed)
-		SET total_performed += 1
-    
-	WHERE User_Account_id = NEW.User_Account_id;
+    UPDATE uso
+    SET uso.total_tracked = uso.total_tracked + 1, -- Increment by exactly 1 per business rule
+        uso.last_tracked_at = GETDATE() -- Update the last_performed_at timestamp
+    FROM User_Salah_Overview uso
+    INNER JOIN inserted i ON uso.User_Account_id = i.User_Account_id
 END;
 GO
 
-CREATE TRIGGER Trigger_Update_Dhikr_Overview on User_Dhikr_Activity
+
+--CREATE TRIGGER Trigger_Increment_User_Salah_Overview_total_tracked
+--ON User_Salah_Activity
+--AFTER INSERT
+--AS
+--BEGIN
+--    -- Update the total_tracked column in User_Salah_Overview for the associated User_Account_id
+--    UPDATE uso
+--    SET uso.total_tracked = uso.total_tracked + 1,
+--        uso.last_tracked_at = GETDATE()
+--    FROM User_Salah_Overview uso
+--    INNER JOIN inserted i ON uso.User_Account_id = i.User_Account_id;
+--END;
+--GO
+
+--TRIGGER FOR TOTAL PERFORMED IN USER DHIKR OVERVIEW BY CHATGPT
+
+CREATE TRIGGER Trigger_Update_User_Dhikr_Overview_total_performed
+ON User_Dhikr_Activity
 AFTER UPDATE
-FOR EACH ROW
+AS
 BEGIN
-    UPDATE User_Dhikr_Overview
-    SET total_performed += 1
-    WHERE User_Account_id = NEW.User_Account_id;
+    -- Check if total_performed was updated
+    IF (UPDATE(total_performed))
+    BEGIN
+        UPDATE udo
+        SET udo.total_performed = udo.total_performed + 1, -- Increment by exactly 1 per business rule
+            udo.last_performed_at = GETDATE() -- Update the last_performed_at timestamp
+        FROM User_Dhikr_Overview udo
+        INNER JOIN inserted i ON udo.User_Account_id = i.User_Account_id
+        INNER JOIN deleted d ON d.User_Account_id = i.User_Account_id
+        WHERE i.total_performed = d.total_performed + 1; -- Ensure the increment is exactly 1
+    END
 END;
+
+GO
+
+CREATE TRIGGER Trigger_Increment_User_Dhikr_Overview_total_performed --when dhikr activity record is made it means there was an activity for space eficiency and many other reasons avoiding creating unnessary data in database!
+ON User_Dhikr_Activity
+AFTER INSERT
+AS
+BEGIN
+    -- Increment total_performed by 1 in User_Dhikr_Overview for the associated User_Account_id
+    UPDATE udo
+    SET udo.total_performed = udo.total_performed + 1,
+        udo.last_performed_at = GETDATE()
+    FROM User_Dhikr_Overview udo
+    INNER JOIN inserted i ON udo.User_Account_id = i.User_Account_id;
+END;
+
+
+--TRIGGER FOR CCREATING USER DHIKR OVERVIEW RECORD FOR NEW USER BY CHATGPT
+
+GO
+
+CREATE TRIGGER Trigger_Create_User_Dhikr_Overview
+ON User_Account
+AFTER INSERT
+AS
+BEGIN
+    -- Insert a record into User_Dhikr_Overview for each new User_Account created
+    INSERT INTO User_Dhikr_Overview (User_Account_id)
+    SELECT id
+    FROM inserted;
+END;
+
+
+--TRIGGER FOR CCREATING USER SALAH OVERVIEW RECORD FOR NEW USER BY CHATGPT
+GO
+
+CREATE TRIGGER Trigger_Create_User_Salah_Overview
+ON User_Account
+AFTER INSERT
+AS
+BEGIN
+    -- Insert a record into User_Dhikr_Overview for each new User_Account created
+    INSERT INTO User_Salah_Overview (User_Account_id)
+    SELECT id
+    FROM inserted;
+END;
+
 --GO
 
 --CREATE TRIGGER Trigger_Update_Salah_Day_Overview
