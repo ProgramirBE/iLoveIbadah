7317327f4712f6dc404efdaace1796c0f260918e
WassimYezeiph <wassim.yezeiph@student.odisee.be>
2024-12-21 17:12:15 +0100

    Commit salat works!

--- Angular Ionic Capacitor/src/app/app-routing.module.ts
+++ Angular Ionic Capacitor/src/app/app-routing.module.ts
@@ -1,40 +1,56 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component'; // Importeer de RegisterComponent
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
+  // Standaard route die naar de login-pagina omleidt
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },  
-  { path: 'register', component: RegisterComponent }, // Voeg de route voor de RegisterComponent toe
+
+  // Route voor login-component
+  { path: 'login', component: LoginComponent },
+
+  // Route voor register-component
+  { path: 'register', component: RegisterComponent },
+
+  // Lazy-loaded route voor de 'home'-module
   {
     path: 'home',
     loadChildren: () =>
-      import('./home/home.module').then((m) => m.HomePageModule), // Laad 'home' module
+      import('./home/home.module').then((m) => m.HomePageModule),
   },
+
+  // Lazy-loaded route voor de 'leaderboard'-module
   {
     path: 'leaderboard',
     loadChildren: () =>
       import('./leaderboard/leaderboard.module').then(
         (m) => m.LeaderboardPageModule
-      ), // Laad 'leaderboard' module
+      ),
   },
+
+  // Lazy-loaded route voor de 'dhikr'-module
   {
     path: 'dhikr',
     loadChildren: () =>
       import('./dhikr/dhikr.module').then((m) => m.DhikrModule),
-  },  
+  },
+
+  // Lazy-loaded route voor de 'salat'-module
   {
     path: 'salat',
     loadChildren: () =>
       import('./salat/salat.module').then((m) => m.SalatModule),
   },
+
+  // Fallback voor niet-bestaande routes
+  { path: '**', redirectTo: 'login' },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }), // Configureer routes met preload strategie
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }), // Gebruik PreloadAllModules voor betere prestaties
   ],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
