d807435b967ccf6ed012a35694b76254e90ff896
WassimYezeiph <wassim.yezeiph@student.odisee.be>
2024-12-27 19:09:06 +0100

    Commit

--- Angular Ionic Capacitor/src/app/presentation/shared/components/home/home.page.ts
+++ Angular Ionic Capacitor/src/app/presentation/shared/components/home/home.page.ts
@@ -6,7 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  username: string = 'Guest'; 
 
   constructor() {}
 
+  setUsername(newUsername: string): void {
+    this.username = newUsername;
+  }
 }
